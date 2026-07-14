4d4e70841b5d69c15599e28fd2e77e7c967c93d3
skareemulla <skareemulla@miraclesoft.com>
2026-07-14 16:01:33 +0530

    firstcommit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users=[{"id":1,"name":"John Doe","email":"john@example.com","age":28,"city":"Hyderabad"},{"id":2,"name":"Alice Smith","email":"alice@example.com","age":25,"city":"Bangalore"},{"id":3,"name":"David Lee","email":"david@example.com","age":31,"city":"Chennai"},{"id":4,"name":"Sarah Johnson","email":"sarah@example.com","age":29,"city":"Mumbai"}]
+
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to my portfolio!"}
+
+
+@app.get("/dataa")
+async def read_data():
+    return users

2fc5eb1bf1aa76e2e156d7b8af9572a00fc763b5
skareemulla <skareemulla@miraclesoft.com>
2026-07-15 15:44:12 +0530

    added mail

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-
+import resend
+import os
 app = FastAPI()
 
 users=[{"id":1,"name":"John vesli","email":"john@example.com","age":28,"city":"Hyderabad"},{"id":2,"name":"Alice smith","email":"alice@example.com","age":25,"city":"Bangalore"},{"id":3,"name":"David Lee","email":"david@example.com","age":31,"city":"Chennai"},{"id":4,"name":"Sarah Johnson","email":"sarah@example.com","age":29,"city":"Mumbai"}]
 
+resend.api_key = os.environ.get("RESEND_API_KEY")
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to my portfolio!"}
@@ -12,3 +14,18 @@ async def read_root():
 @app.get("/dataa")
 async def read_data():
     return users
+
+@app.post("/send-email")
+async def send_email(name:str, mailfrom:str,subject:str,message:str, mailto:str="kareemsheik133@gmail.com"):
+    try:
+        email = resend.Email(
+            from_email=mailfrom,
+            to=[mailto],
+            subject=subject,
+            text=message
+        )
+        response = email.send()
+        return {"message": "Email sent successfully", "response": response}
+    except Exception as e:
+        return {"error": str(e)}
+     
